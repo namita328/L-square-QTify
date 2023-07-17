71956d77237ee9e5fcf1abceb924184b22ce3ac9
namita <namita328>
2023-07-17 15:20:06 +0530

    tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2
+          Learn React3
         </a>
       </header>
     </div>
